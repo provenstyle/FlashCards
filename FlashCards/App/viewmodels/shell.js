3ee95196eb7e46505fe90b2c4b56304db33297e4
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-01-16 10:18:44 -0600

    Cleaning up the catalog html

--- FlashCards/App/viewmodels/shell.js
+++ FlashCards/App/viewmodels/shell.js
@@ -4,7 +4,7 @@
         activate: function () {
             router.map([
                 { route: ['catalog', '' ], title: 'Catalog', moduleId: 'viewmodels/catalog', nav: true },
-                { route: 'about', title: 'About', moduleId: 'viewmodels/about', nav: false }
+                { route: 'about', title: 'About', moduleId: 'viewmodels/about', nav: true }
             ]).buildNavigationModel();
 
             return router.activate();
