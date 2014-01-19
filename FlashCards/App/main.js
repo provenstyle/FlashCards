6467c16eacdcc12ad4d179b0b67c4b387f0ac601
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-01-19 09:48:44 -0600

    Fixing title

--- FlashCards/App/main.js
+++ FlashCards/App/main.js
@@ -15,7 +15,7 @@ define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (s
     system.debug(true);
     //>>excludeEnd("build");
 
-    app.title = 'Durandal Starter Kit';
+    app.title = 'Flash Cards';
 
     app.configurePlugins({
         router: true,
