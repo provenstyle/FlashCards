65154665bb652bf4f06d6cffcd6c34b4b3ded996
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-01-18 18:52:59 -0600

    Showing card cound and name

--- FlashCards/App/mockData/requirejs.js
+++ FlashCards/App/mockData/requirejs.js
@@ -1,8 +1,8 @@
 ﻿define(['models/card'], function(card) {
 
     var cards = [
-        new card("True or False:", "RequireJS uses r.js to bundle and minify your AMD modules.", "True:", ""),
-        new card("True or False:", "AMD stands for Asynchronous Module Definition.",             "True:", "")
+        new card("True or False:", "RequireJS uses r.js to bundle and minify your AMD modules.", "True", ""),
+        new card("True or False:", "AMD stands for Asynchronous Module Definition.",             "True", "")
     ];
 
     return {
